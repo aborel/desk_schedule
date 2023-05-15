9e30aa07d04f781bb4fb30aeb3b8325443c31572
Alain Borel <alain.borel@epfl.ch>
2023-05-15 07:59:40 +0200

    Input file selection working

--- desk_schedule_ui.py
+++ desk_schedule_ui.py
@@ -17,6 +17,8 @@ version = "1.0"
 
 error_file = "desk_schedule_errors.txt"
 
+horaires = None
+absences = None
 
 def log_message(message):
     f_err = open(error_file, "a")
@@ -24,47 +26,43 @@ def log_message(message):
     f_err.close()
 
 
-def get_horaire_file(tkroot, width_chars, choosefile):
+def get_horaire_file():
+    global horaires
     filetypes = (
         ('text files', '*.xlsx'),
         ('All files', '*.*')
     )
     d_title = "Choose the Horaires XLSX file"
-    choosefile = filedialog.askopenfilename(initialdir=Path.home(), filetypes=filetypes, title=d_title)
-    print(choosefile)
+    horaires = filedialog.askopenfilename(initialdir=Path.home(), filetypes=filetypes, title=d_title)
 
-def get_absence_file(tkroot, width_chars, choosefile):
+
+def get_absence_file():
+    global absences
     filetypes = (
         ('text files', '*.html'),
         ('All files', '*.*')
     )
     d_title = "Choose the absences.epfl.ch file (saved HTML)"
-    choosefile = filedialog.askopenfilename(initialdir=Path.home(), filetypes=filetypes, title=d_title)
-    print(choosefile)
+    absences = filedialog.askopenfilename(initialdir=Path.home(), filetypes=filetypes, title=d_title)
 
 
-def run_desk_schedule(tkroot, width_chars, check_zips):
-    # Clear all existing text messages
-    do_zips = bool(check_zips.get())
-    for label in tkroot.winfo_children():
-        if type(label) is Label:
-            label.destroy()
-    
+def run_desk_schedule(tkroot, width_chars):
+    global horaires
+    global absences
+    print(horaires)
+    print(absences)    
 
     error_message = f"There were errors or warnings during processing:\ncheck {error_file} for information."
-    error_file_header = "This is the acouachecksum log for errors and warnings. Do not archive.\n"
-
-    d_title = "Select your ingestion folder"
-    choosedir = filedialog.askdirectory(initialdir=Path.home(), title=d_title)
-    if choosedir == '' or not os.path.exists(choosedir):
-        return
-    os.chdir(choosedir)
+    error_file_header = "This is the desk_schedule log for errors and warnings. Do not archive.\n"
 
     # delete existing logfile unless it doesn't exist
     try:
         os.remove(error_file)
     except OSError:
         pass
+
+    f_err = open(error_file, "w")
+    f_err.close()
 
  
 
@@ -88,12 +86,10 @@ root.wm_title("EPFL Library desk schedule v" + version)
 width = 400
 width_chars = int(1.7*width / current_font.actual()['size'])
 root.geometry(f'{width}x250+1000+300')
-horaires_var = StringVar()
-Button(root, text="Sélection du fichier des horaires", command=partial(get_horaire_file, root, width_chars, horaires_var)).pack()
 
-absences_var = StringVar()
-Button(root, text="Sélection du fichier des absences (optionnel)", command=partial(get_absence_file, root, width_chars, absences_var)).pack()
+Button(root, text="Sélection du fichier des horaires", command=get_horaire_file).pack()
+Button(root, text="Sélection du fichier des absences (optionnel)", command=get_absence_file).pack()
 
 button_label = 'Générer planning'
-Button(root, text=button_label, command=partial(run_desk_schedule, root, width_chars, horaires_var, absences_var)).pack()
+Button(root, text=button_label, command=partial(run_desk_schedule, root, width_chars)).pack()
 root.mainloop()
