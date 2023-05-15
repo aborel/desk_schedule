c77e0c7218edca1516f98b455f05eff4631846da
Alain Borel <alain.borel@epfl.ch>
2023-05-15 08:01:18 +0200

    deleted unnecessary file

--- download_file.py
+++
@@ -1,8 +0,0 @@
-import gdown
-import pandas as pd
-
-url = "https://drive.google.com/uc?id=15BPH7-3GGWBfXPJQ3stkT6SHQECbT-pt"
-output = "shifts.csv"
-gdown.download(url, output, quiet=False)
-
-df = pd.read_csv("shifts.csv", index_col=0)
