8fde5f3bd28a52940c518ab3c38e84bdd60f804b
Alain Borel <alain.borel@epfl.ch>
2023-05-15 07:59:40 +0200

    moving parse_absences to absencesV2

--- parse_absences.py
+++ parse_absences.py
@@ -1,60 +1,99 @@
 import re
-import os
-import pathlib
+import sys
 
 from bs4 import BeautifulSoup
 import dateparser
 
 # From https://absences.epfl.ch/cgi-bin/abs/lang=en/menu=plannings/listAbs?week=1
 # or https://absences.epfl.ch/cgi-bin/abs/lang=en/instID=1459/menu=plannings/listAbs?date=20230227&week=1
-# The week argument appears to have no effect
 
-def parse_absence_html(ls):
-    HTMLFile = open(os.path.join(str(ls.parents[0]), ls.name), "r")
+def get_all_text(node):
+    if node.nodeType == node.TEXT_NODE:
+        return node.text
+    else:
+        text_string = ""
+        for child_node in node.childNodes:
+            text_string += get_all_text(child_node)
+    return text_string
+
+
+def list_divs(node, ntabs=0):
+    all_subdivs = node.find_all('div')
+    print(len(all_subdivs))
+    subdivs = [div for div in all_subdivs if len(div.find_all('div')) > 0]
+    for subdiv in subdivs:
+        list_divs(subdiv, ntabs=ntabs+1)
+    print('\t'*ntabs + f'{len(subdivs)} subdivs')
+
+
+def parse_absences(htmlfile):
+    HTMLFile = open(htmlfile, "r")
     index = HTMLFile.read()
 
     S = BeautifulSoup(index, 'lxml')
     try:
-        absence_table = S.find_all('table')[0]
-        for row in absence_table.find_all('tr'):
-            print('\n')
-            cells = list(row.find_all('td'))[0:5]
-            if len(cells) == 5:
-                del cells[-2]
-                if not cells[-1].decode_contents() == 'télétravail' and not cells[-1].decode_contents() == 'télétravail':
-                    elements = []
-                    for idx, cell in enumerate(cells):
-                        try:
-                            s = cell.find_all('a')[0].decode_contents()
-                        except IndexError:
-                            s = cell.decode_contents()
-                        s = s.replace(' ', ' ').lower()
-                        print('original:', s)
-                        s = s.replace(' ', ' ')
-                        s = s.replace('early afternoon', '1300')
-                        s = s.replace('début après-midi', '1300')
-                        s = s.replace('end of afternoon', '2000')
-                        s = s.replace('fin après-midi', '2000')
-                        s = s.replace('early morning', '0800')
-                        s = s.replace('début matinée', '0800')
-                        s = s.replace('end of morning', '1159')
-                        s = s.replace('midi', '1159')
-                        print('processed:', s)
-                        if len(re.findall('[0-9]{4} [0-9]{4}', s)) > 0:
-                            # print('Found one!')
-                            s = s[0:-2] + ':' + s[-2:]
-                            elements.append(dateparser.parse(s))
-                        else:
-                            elements.append(s)
-                    print(elements)
-        print('\n')
-    except IndexError:
-        print('no table found?')
+        tables = S.find_all('table')
+        print(f'Found {len(tables)} tables in the page.')
+
+        for idx, t in enumerate(tables):
+            subtables = t.find_all('table')
+            rows = t.find_all('tr')
+            print(f'Table {idx} has {len(rows)} rows and contains {len(subtables)} sub-tables')
+            print('Is FV in there?', t.decode_contents().find('Varrato'))
+            print('Is Holidays in there?', t.decode_contents().find('Holidays'))
+            for ridx, r in enumerate(rows):
+                cols = r.find_all('td')
+                if len(cols) == 1:
+                    if  get_all_text(cols[0]) is None:
+                        print(f'Weird column:')
+                    else:
+                        pass
+                else:
+                    print(f'\tRow {ridx} has {len(cols)} columns')
+
+        print('---------')
+        people_table = tables[3]
+        print('Is FV really in there?', people_table.decode_contents().find('Varrato'))
+        people_rows = people_table.find_all('tr')
+        librarians = [get_all_text(row).strip() for row in people_rows]
+        for row in people_rows:
+            cols = row.find_all('td')
+            #print()
+            #print('---')
+
+        fcDayThs = [th for th in S.find_all('th', class_='fc-day') if th.get('colspan') == '1']
+        for th in fcDayThs:
+            print([th.get('colspan')], get_all_text(th), th.get('data-date'))
+        fcDayTds = S.find_all('td', class_='fc-day')
+        print(f'Found {len(fcDayTds)} days')
+        fcDayTds = S.find_all('td', class_='fc-day')
+
+        absence_table = tables[5]
+
+        #print(absence_table.decode_contents().find('Borel'))
+        #print(absence_table.decode_contents().find('Varrato'))
+        absence_rows = absence_table.find_all('tr')
+        for ridx, row in enumerate(absence_rows):
+            cells = row.find_all('td')
+            print(len(cells))
+            if (len(cells) > 0):
+                print(cells[0].class_)
+                #print(cells[0].decode_contents())
+                all_divs = cells[0].find_all('div')
+                inner_divs = [div for div in all_divs if len(div.find_all('div')) == 0]
+                events = [div for div in all_divs if 'fc-timeline-event-harness' in div.get('class')]
+                print(f'{len(events)} events')
+                print(librarians[ridx], len(inner_divs), [(div.get('class'), get_all_text(div).strip(), div.get('style')) for div in events])
+
+    except IndexError as e:
+        print('no table found?', e)
+    return None
 
 
 if __name__ == "__main__":
-    absence_folder = './absences'
-    absence_files = pathlib.Path(absence_folder).glob('*.html')
+    if len(sys.argv) > 1:
+        htmlfile = sys.argv[1]
+    else:
+        htmlfile = "absences.html"
 
-    for htmlfile in absence_files:
-        parse_absence_html(htmlfile)
+    output = parse_absences(htmlfile)
